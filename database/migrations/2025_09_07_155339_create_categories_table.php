cec181d84985462835894e300e57730d08734103
Irfan Haqim <irfanhaqim4039@gmail.com>
2025-09-08 00:00:47 +0800

    feat: migration

--- database/migrations/2025_09_07_155339_create_categories_table.php
+++ database/migrations/2025_09_07_155339_create_categories_table.php
@@ -11,13 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('tasks', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
-            $table->text('description')->nullable();
-            $table->date('due_date')->nullable();
-            $table->boolean('status')->default(0);
-            $table->foreignId('category_id')->nullable()->constrained('categories')->nullOnDelete();
+            $table->string('name')->unique();
             $table->timestamps();
         });
     }
@@ -27,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('tasks');
+        Schema::dropIfExists('categories');
     }
 };
